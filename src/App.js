3f11b0efa39491e4788bc05353302dafdbc7ed27
Arti <artem.kort@stiftsgymnasium.de>
2025-06-20 16:24:51 +0200

    Define site data types

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import data from "./content.json";
+import data from "./contentDefaults.json";
 
 function App() {
     return (
         <HashRouter>
             <Routes>
                 <Route index element={<Home data={data} />} />
+                {data.pages.map((page) => (
+                    <Route path={page.slug} element={page.content} />
+                ))}
             </Routes>
         </HashRouter>
     );
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,8 +1,44 @@
+interface EmbedImage {
+    src: string;
+    alt?: string;
+    width?: number;
+    height?: number;
+    key: string;
+}
+interface Page {
+    title: string;
+    slug: string;
+    content: string;
+    description?: string;
+    images?: EmbedImage[];
+}
 export interface SiteData {
     siteTitle?: string;
     siteDescription?: string;
+    keywords?: string[];
+    author?: string;
+    language?: string;
+    url?: string;
+    socialMedia?: {
+        present: boolean;
+        twitter?: string;
+        facebook?: string;
+        instagram?: string;
+        linkedin?: string;
+        github?: string;
+        youtube?: string;
+        tiktok?: string;
+        pinterest?: string;
+        reddit?: string;
+    };
+    contactEmail?: string;
+    pages?: Page[];
 }
-declare module "*.json" {
+declare module "content.json" {
+    const data: SiteData;
+    export default data;
+}
+declare module "contentDefaults.json" {
     const data: SiteData;
     export default data;
 }
