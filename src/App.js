5a56b040ab5b0b431daa13d2b8c2f4611c5f0d38
Arti <artem.kort@stiftsgymnasium.de>
2025-06-21 10:25:17 +0200

    Set default data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import data from "./contentDefaults.json";
+import data from "./content.json";
+import defaults from "./contentDefaults.json";
 
 function App() {
+    // loop through the possible site data and set defaults
+    let validData = data;
+    Object.keys(defaults).forEach((key) => {
+        validData[key] = data[key] || defaults[key];
+    });
+    console.log("Valid Data:", validData);
     return (
         <HashRouter>
             <Routes>
-                <Route index element={<Home data={data} />} />
-                {data.pages.map((page) => (
+                <Route index element={<Home data={validData} />} />
+                {validData.pages.map((page) => (
                     <Route path={page.slug} element={page.content} />
                 ))}
             </Routes>
